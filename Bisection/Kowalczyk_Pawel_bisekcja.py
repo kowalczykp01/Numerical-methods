efad9b7e0ef229b959595d35689fa1f90c96f1c2
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2022-05-22 22:26:20 +0200

    Final version of bisection method

---
+++ Bisection/Kowalczyk_Pawel_bisekcja.py
@@ -0,0 +1,20 @@
+
+def function(x):
+    result = eval("x*x*x - x - 2")
+    return result
+
+def kowalczyk_pawel_bisekcja(a, b, eps):
+    c = (a+b)/2
+    if (b - a) <= eps:
+        return c
+    else:
+        while (b - a) > eps:
+            if function(a)*function(c) < 0:
+                b = c
+                c = (a+b)/2
+            else:
+                a = c
+                c = (a+b)/2
+        return c
+
+print(kowalczyk_pawel_bisekcja(1,2, 0.0001))
